67f39497c5b4866e4a7293f3526f457e59f08a95
Kai Tait <ktait@auxita.com>
2021-01-15 11:46:22 +1000

    Updates

---
+++ precache-manifest.c35bf53ffae66eb3480c067b54ef3eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49367c30d1c8020dc21c83163b22da54",
+    "url": "/typescript-formik-i18n/index.html"
+  },
+  {
+    "revision": "5f3a7f6dd2a044f91844",
+    "url": "/typescript-formik-i18n/static/css/main.83246e79.chunk.css"
+  },
+  {
+    "revision": "2f0710e32a04e0461c1b",
+    "url": "/typescript-formik-i18n/static/js/2.e912b63b.chunk.js"
+  },
+  {
+    "revision": "5f3a7f6dd2a044f91844",
+    "url": "/typescript-formik-i18n/static/js/main.4879edf1.chunk.js"
+  },
+  {
+    "revision": "544e9b3f4dfd15bb20b3",
+    "url": "/typescript-formik-i18n/static/js/runtime-main.a27e9eb3.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/typescript-formik-i18n/static/media/getFetch.013f0b90.cjs"
+  }
+]);
